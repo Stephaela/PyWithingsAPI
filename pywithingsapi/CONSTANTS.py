540c6f7dcfd96d352b49ae2ad103b88815c3604c
Stephaela <stefanie.klemm@posteo.de>
2024-09-08 17:54:32 +0200

    add CONSTANTS.py

---
+++ pywithingsapi/CONSTANTS.py
@@ -0,0 +1,11 @@
+"""
+CONSTANTS.py module
+
+This module contains constants used in the PyWithingsAPI project.
+"""
+
+import os
+
+STANDARD_SCOPE = "user.info,user.metrics,user.activity"
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
